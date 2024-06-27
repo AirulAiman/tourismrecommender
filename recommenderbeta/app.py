ce68ef13915645daf8b976bad66fbfcb8a515b16
AirulAiman <136466706+AirulAiman@users.noreply.github.com>
2024-06-27 15:07:38 +0800

    Update app.py

--- recommenderbeta/app.py
+++ recommenderbeta/app.py
@@ -2,7 +2,6 @@ import pickle
 import streamlit as st
 import pandas as pd  # Import pandas
 import requests
-import path
 import sys
 
 st.header("Tourism Recommendation System Using Machine Learning")
